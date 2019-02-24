b32be1e20d7cf16cb3081c037a5ab152a46ab752
Anna Pollák <tintagomba@yahoo.co.uk>
2019-02-24 10:55:51 +0100

    fibonacci sequence project added, solved

---
+++ Dojo/KeyValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dojos
+{
+    public class KeyValue
+    {
+        public int Key;
+        public string Value;
+    }
+}
---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            for (int i = 0; i < 20; i++)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach (var item in fibonacciNumbers) Console.WriteLine(item);
+        }
+    }
+}
