7fe5eb3f5a924019deac9af4e06a6c83d3552e4d
Anna Pollák <tintagomba@yahoo.co.uk>
2019-03-06 22:07:50 +0100

    first solution added

--- PrimeNumberFinder/Program.cs
+++ PrimeNumberFinder/Program.cs
@@ -6,33 +6,26 @@ using System.Threading.Tasks;
 
 namespace PrimeNumberFinder
 {
-    class PrimeChecker
+    class Program
     {
-        public int isItPrime = 4;
 
-        public bool CheckIfDivisibleByTwoThreeFive()
-        {
-
-            if (isItPrime != 1 &&
-                isItPrime != 2 &&
-                isItPrime != 3 &&
-                isItPrime != 5)
-            {
-                if (isItPrime % 2 == 0 ||
-                    isItPrime % 3 == 0 ||
-                    isItPrime % 5 == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
 
         static void Main(string[] args)
         {
-            PrimeChecker check = new PrimeChecker();
-            Console.WriteLine("Is it prime? " + check.CheckIfDivisibleByTwoThreeFive());
-           
+            Console.WriteLine(CheckIfPrime(73454371));
+
+            bool CheckIfPrime(int num)
+            {
+                for (int i = 2; i < num/2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.WriteLine($"{num} is dividible by {i}");
+                        return false;
+                    }
+                }
+                return true;
+            }
         }
     }
 }
