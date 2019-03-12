48262c967130eb287d24ebb01fd78bfd8bce7a28
Anna Pollák <tintagomba@yahoo.co.uk>
2019-03-12 14:17:34 +0100

    checking if a string contains unique characters method implemented

--- ArraysAndStrings/Program.cs
+++ ArraysAndStrings/Program.cs
@@ -8,8 +8,37 @@ namespace ArraysAndStrings
 {
     class Program
     {
+        //help
+        public bool IsUnique(string word)
+        {
+            for (int index = 0; index < word.Length; index++)
+            {
+                for (int nextIndex = index + 1; nextIndex < word.Length; nextIndex++)
+                {
+                    if (word[index] == word[nextIndex])
+                    {
+                        return false;
+                    }
+                } 
+            }
+            return true;
+        }
+
+        public bool CheckPermutation(string word1, string word2)
+        {
+            return true;
+        }
+
+        public string Urlify(string stringWithSpaces)
+        {
+            return "fed";
+        }
+
         static void Main(string[] args)
         {
+            Program start = new Program();
+            Console.WriteLine(start.IsUnique("qwertzuiopélkjhgfdsyxcvbnm,."));
+
         }
     }
 }
